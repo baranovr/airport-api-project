f23c355a3815348924c2d528fa8bf20b49374a6e
rusip <rusipbox@gmail.com>
2024-05-04 19:53:47 +0300

    filled out admin files

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Airport,
+    Airplane,
+    AirplaneType,
+    Flight,
+    Route,
+    Crew,
+    Ticket,
+    Order
+)
+
+
+admin.site.register(Airport)
+admin.site.register(Airplane)
+admin.site.register(AirplaneType)
+admin.site.register(Flight)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Ticket)
+admin.site.register(Order)
