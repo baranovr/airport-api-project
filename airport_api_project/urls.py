f31f149e9b9f99470fd4b662cf6054bc1b99c0e7
rusip <rusipbox@gmail.com>
2024-05-12 23:47:58 +0300

    added paths to spectacular schema in project urls

--- airport_api_project/urls.py
+++ airport_api_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, \
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
     SpectacularRedocView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
